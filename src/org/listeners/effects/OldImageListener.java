c812832f5a5b6e5f0e40c2c7c9e34b2267ab5251
Lucas Amaral <lucasamaral1@gmail.com>
2013-12-01 02:26:50 -0200

    slider para escolher o envelhecimento

--- src/org/listeners/effects/OldImageListener.java
+++ src/org/listeners/effects/OldImageListener.java
@@ -3,7 +3,11 @@ package org.listeners.effects;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 import org.gui.AppScreen;
+import org.gui.SepiaSliderDialog;
 import org.transform.Effects;
 
 public class OldImageListener implements ActionListener {
@@ -18,7 +22,14 @@ public class OldImageListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Effects effect = new Effects(app.getImage());
-		this.app.setImage(effect.oldImage(5));
+		SepiaSliderDialog sliderDialog = new SepiaSliderDialog();
+		JDialog dialog = sliderDialog.createDialog(app, "Intesidade do Envelhecimento");
+		dialog.setVisible(true);
+		String value = sliderDialog.getInputValue().toString();
+		if(sliderDialog.getInputValue() == JOptionPane.UNINITIALIZED_VALUE)
+			this.app.setImage(effect.oldImage(5));
+		else
+			this.app.setImage(effect.oldImage(Integer.parseInt(value)));
 	}
 
 }
