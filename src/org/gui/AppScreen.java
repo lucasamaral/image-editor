5578458cf049da08a64ba2c474f900e227f57f75
juandecastro <juancastro.pe@gmail.com>
2013-12-01 02:40:36 -0200

    rotate

--- src/org/gui/AppScreen.java
+++ src/org/gui/AppScreen.java
@@ -14,6 +14,7 @@ import javax.swing.JMenuBar;
 import javax.swing.WindowConstants;
 
 import org.transform.Effects;
+import org.transform.ImageTransform;
 
 public class AppScreen extends JFrame {
 
